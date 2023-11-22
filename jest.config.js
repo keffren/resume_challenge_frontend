728faaa5af8d2297c6eba97f6d98f3c4f20b729b
keffren <sernamateouk@gmail.com>
2023-11-22 00:22:21 +0000

    Add the jest-puppeteer package for the action test

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: "jest-puppeteer",
+};
+
+module.exports = {
+    launch: {
+      headless: process.env.CI === "true",
+      ignoreDefaultArgs: ["--disable-extensions"],
+      args: ["--no-sandbox"],
+      executablePath: "chrome.exe",
+    },
+};
